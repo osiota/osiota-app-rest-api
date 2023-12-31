caa1ab09d00561bb6b201ecdbaddaf77f81ae34d
Simon Walz <contact@simonwalz.de>
2023-12-31 01:55:44 +0100

    Adaption of OPENAPI, Error handling

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 
 var express = require('express');
 
+class HTTPError extends Error {
+	constructor(status, message) {
+		super(message);
+		this.name = 'HTTPError';
+		this.status = status;
+	}
+}
+
+
+var actions = {
+	"on": ["set", true],
+	"off": ["set", false],
+	"toggle": ["toggle"],
+};
+
+function map_action(action) {
+	if (typeof action !== "string" ||
+			typeof actions[action] === "undefined") {
+		throw new HTTPError(400, "action not found");
+	}
+	return JSON.parse(JSON.stringify(actions[action]));
+}
+
 exports.init = function(node, app_config, main, host_info) {
 	if (typeof app_config !== "object") {
 		app_config = {};
@@ -33,16 +56,12 @@ exports.init = function(node, app_config, main, host_info) {
 		try {
 			path = decodeURIComponent(path);
 		} catch(err) {
-			res.status(400);
-			res.send("Exception: " + e.stack || e);
-			return;
+			throw new HTTPError(400, "Path could not be decoded");
 		}
 		var n = main.node(path);
 		console.log("get", path, n.value);
 		if (!n.metadata) {
-			res.status(404);
-			res.send("Node not found.");
-			return;
+			throw new HTTPError(404, "Node not found");
 		}
 		res.set('Content-Type', 'application/json');
 		res.send(
@@ -56,60 +75,69 @@ exports.init = function(node, app_config, main, host_info) {
 		try {
 			path = decodeURIComponent(path);
 		} catch(e) {
-			res.status(400);
-			res.send("Exception: " + e.stack || e);
-			return;
+			throw new HTTPError(400, "URL could not be decoded.");
 		}
 		console.log("post", path);
 		var n = main.node(path);
 		if (!n.metadata) {
-			res.status(404);
-			res.send("Node not found.");
-			return;
+			throw new HTTPError(404, "Node not found.");
 		}
-		if (req.query && typeof req.query.method === "string") {
-			rpc_method = req.query.method;
-		}
+		//if (req.query && typeof req.query.method === "string") {
+		//	rpc_method = req.query.method;
+		//}
 		try {
-			var args = JSON.parse(req.body);
-			if (rpc_method) {
-				args = [rpc_method, args];
+			var body = JSON.parse(req.body);
+		} catch (err) {
+			throw new HTTPError(422, "JSON body could not be parsed.");
+		}
+		if (typeof body !== "object" ||
+				body === null) {
+			throw new HTTPError(422, "No JSON body");
+		}
+		let args = [];
+		if (typeof body.action === "string") {
+			args = map_action(body.action);
+		} else {
+			if (typeof body.method !== "string") {
+				throw new HTTPError(400, "Missing property action or method");
 			}
-			if (!Array.isArray(args)) {
-				res.status(400);
-				res.send("Argument is not an array");
-				return;
+			if (Array.isArray(body.arguments)) {
+				args = body.arguments;
 			}
-			args.push(function(err) {
-				var args = Array.prototype.slice.call(
-					arguments, 1);
-				if (err) {
-					res.status(500);
-					args = {
-						"error": err,
-						"data": args
-					};
-				}
-				res.set('Content-Type', 'application/json');
-				res.send(JSON.stringify(args)+"\n");
-			});
-			var n = main.node(path);
-			console.log("rpc:", args);
-			// Add request-object
-			if (args[0].match(/^req_/)) {
-				args.splice(1, 0, req);
+			args.unshift(body.method);
+		}
+		args.push(function(err) {
+			var data = Array.prototype.slice.call(
+				arguments, 1);
+			console.log("REST Answer", err, data);
+			if (err) {
+				res.status(500);
+				data = {
+					"error": err,
+					"data": data
+				};
 			}
-			n.rpc.apply(n, args);
-		} catch(e) {
-			res.status(422);
-			var data = {"error": e.stack || e };
 			res.set('Content-Type', 'application/json');
 			res.send(JSON.stringify(data)+"\n");
+		});
+		console.log("rpc:", args);
+		// Add request-object
+		if (body.method && body.method.match(/^req_/)) {
+			args.splice(1, 0, req);
 		}
+		n.rpc.apply(n, args);
 	});
 
 	var app = express();
 	app.use(base_path, router);
+
+	// error handler:
+	app.use(function (err, req, res, next) {
+		console.error("REST Error:", err.stack)
+		res
+			.status(err.status)
+			.send("Error: "+err.message);
+	});
 
 	var server = app.listen(app_config.server, function () {
 	    console.log("Listening on port %s...", server.address().port);
